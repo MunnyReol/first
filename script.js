b70ef84957d5b9c7352db814a86dfc00f6982515
MunnyReol <59450380+MunnyReol@users.noreply.github.com>
2020-02-17 10:27:09 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+body {
+    background-color:orange;
+    text-align:center;
+}
+
+#click {  
+   color:blue;
+    border-color:red;
+    background-color:cyan;
+}
+
+.name{
+    color:green;
+    text-align:center;
+    font-size:25px;
+}
+p{
+    text-align:center ;
+    font-size:200%;
+    color:yellow;
+}
